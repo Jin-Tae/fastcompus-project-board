f2d9dcd2756a318c07b23f1f1138bd1ea55d5321
evan.kim <evan.kim1992@gmail.com>
2023-12-30 21:44:27 +0900

    #2-스프링부트 프로젝트 시작
    스프링부트 프로젝트 초기화
    들어간 초기 기술은 :
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ fastcompus-project-board/src/main/java/com/fastcampus/projectboard/FastcompusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcompusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcompusProjectBoardApplication.class, args);
+    }
+
+}
